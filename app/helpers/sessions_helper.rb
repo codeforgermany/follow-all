299d550b69f4dfd6db78ed033f4bb588df2d50da
Drew <drew.r.wilson@gmail.com>
2015-02-22 13:32:31 +0100

    quick updates for cfgermany

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,11 @@
 module SessionsHelper
-  FIRST_YEAR = 2011
 
   def lists
-    list_owner = 'codeforamerica'
+    list_owner = 'codeforde'
     lists = []
-    lists << 'team'
-    (FIRST_YEAR..Date.today.year).each do |year|
-      lists << "fellows-#{year}"
-    end
-    lists << 'staff'
+    lists << 'code-for-de-labs'
+    lists << 'code-for-de-team'
+    lists << 'advisory-board'
     lists.collect do |list|
       [[list_owner, list].join('/')]
     end
